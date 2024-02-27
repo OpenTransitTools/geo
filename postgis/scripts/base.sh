b391b975f7628738c1a8ca5e7492df2a0076e925
Frank Purcell <fxpurcell@gmail.com>
2024-02-27 15:43:59 -0800

    feat: load + export (dump) script

--- postgis/scripts/base.sh
+++ postgis/scripts/base.sh
@@ -3,14 +3,17 @@ TAR_DIR=~/gtfsdb/cache/
 db_url=$1
 def_db=${2:-postgres}
 
-psql="docker exec -u $def_db -it db psql"
-pg_restore="docker exec -i -u $def_db db pg_restore"
+docker_exe="docker exec -i -u $def_db -it db"
+psql="$docker_exe psql"
+pg_restore="$docker_exe pg_restore"
+pg_dump="$docker_exe pg_dump"
 
 # IMPORTANT: there are are python configs for user, pass and db in loader/config/app.ini, which also need to change
 user=ott
 pass=ott
 db=ott
 osm_db=osm
+otp_url=postgresql://$user:$pass@127.0.0.1:5432/$db
 
 # use URL if we get content on the cmd line (default to docker url when no ://)
 if  [[ "$db_url" != "" ]] && [[ "$db_url" != *"://"* ]]; then
---
+++ postgis/scripts/load.sh
@@ -0,0 +1,29 @@
+##
+## crete OTT spatial db for OTT
+##
+DIR=`dirname $0`
+. $DIR/base.sh
+
+# put loader's bin dir here
+ln -s ~/rtp/loader/bin . > /dev/null 2>&1
+
+FEEDS_DIR="$HOME/gtfs/cache"
+for f in ${FEEDS_DIR}/*gtfs.zip
+do
+  # rename '../AGENCY_NAME.gtfs.zip' to 'agency_name' 
+  name=${f#$FEEDS_DIR/}
+  name=${name%.gtfs.zip}
+  name=${name,,}
+
+  cmd="bin/gtfsdb-load -c -ct -g -d $otp_url -s ${name} ${f}"
+  echo $cmd
+  eval $cmd
+
+  sleep 1
+
+  dump="$pg_dump -n ${name} $db -F t > ${FEEDS_DIR}/${name}.jar"
+  echo $dump
+  eval $dump
+  
+  echo; echo;
+done
