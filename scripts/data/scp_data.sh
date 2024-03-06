d71a4e16dc52063438bc96c15e20af2bdc5ab39c
Frank Purcell <fxpurcell@gmail.com>
2024-03-05 16:12:26 -0800

    feat: copy gtfs .tar files for postgis to other servers

--- scripts/data/scp_data.sh
+++ scripts/data/scp_data.sh
@@ -10,16 +10,20 @@ unset MACHINES
 data_dir="~/rtp/map_server/data_dir"
 db_snaps=`ls ~/gtfs/cache/*ar`
 
-
 for m in $MACHINES
 do
-  cmd="scp -r $data_dir $m:$PWD/geoserver/"
+  cmd="ssh ${m} 'rm -rf ~/tmp/gtfs; cp -r ~/gtfs /tmp/; rm -rf ~/gtfs; mkdir ~/gtfs'"
+  echo $cmd
+  eval $cmd
+
+  cmd="scp -r $data_dir ${m}:${PWD}/geoserver/"
   echo $cmd
   #eval $cmd
 
   for d in $db_snaps
   do
-    cmd="scp -r $d $m:$/gtfs/"
+    cmd="scp $d ${m}:~/gtfs/"
     echo $cmd
+    #eval $cmd
   done
 done
