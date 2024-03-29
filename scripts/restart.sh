a55df5bcb4f0a7c21bcf8aab98368819ab0af190
Frank Purcell <fxpurcell@gmail.com>
2024-03-29 14:25:38 -0700

    fix: bit of cleanup

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,13 +2,12 @@ date
 rm -f ~/geo/*/nohup.out
 
 cd ~/geo/geoserver
-docker-compose down; sleep 2
+docker-compose down -v; sleep 2
 
 cd ~/geo/postgis
-docker-compose down; sleep 5
+docker-compose down -v; sleep 5
 
-
-docker network prune -f
+docker network prune -f; sleep 2
 
 
 echo $PWD
