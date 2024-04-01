cd5afe90d5a95c5a2e5d1b40e27665c871298171
Frank Purcell <fxpurcell@gmail.com>
2024-04-01 14:53:40 -0700

    feat: add a file loader

---
+++ scripts/db/file.sh
@@ -0,0 +1,15 @@
+DIR=`dirname $0`
+. $DIR/../base.sh
+
+sql_file=${1:-"NOPE"}
+
+if [ -f $sql_file ]; then
+  echo loading .sql file: $sql_file
+  r="$pg_restore -d ${db_url}${db} < $sql_file"
+  echo $r
+  #eval $r
+elif [ $sql_file == "NOPE" ]; then
+  echo "file.sh <sql commands .sql>"
+else
+  echo "$sql_file doesn't exist"
+fi
