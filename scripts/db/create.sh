c81b1cff0e4929583539bc284df5095f8cba3bcf
Frank Purcell <fxpurcell@gmail.com>
2024-05-10 11:30:09 -0700

    fix: clean up scripts

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -7,8 +7,8 @@ DIR=`dirname $0`
 ## create user (default is user='ott' with pass='ott' -- change in ./base.sh)
 $psql -c "CREATE USER ${user} WITH PASSWORD '${pass}';"
 
-# create ott and osm DBs
-for d in $db $osm_db
+# create ott DB (and formally osm $osm_db)
+for d in $db 
 do
   $psql -c "CREATE DATABASE ${d} WITH OWNER ${user};"
   $psql $d -c "CREATE EXTENSION postgis;"
