20eb045532d0e9d01f1bdfb55fd607ecec32cb83
Frank Purcell <fxpurcell@gmail.com>
2024-06-24 21:57:41 -0700

    fix: run geoserver in bg

--- scripts/db/reload.sh
+++ scripts/db/reload.sh
@@ -3,4 +3,20 @@
 ##
 DIR=`dirname $0`
 . $DIR/../base.sh
-$psql ott
+
+cd ~/geo/geoserver
+docker-compose down
+wait 4
+
+cd ~/geo/
+$DIR/drop.sh
+sleep 1
+$DIR/create.sh
+sleep 1
+$DIR/load.sh
+sleep 4
+
+$DIR/file.sh gs_sql_view.txt
+
+cd ~/geo/geoserver
+nohup docker-compose up &
