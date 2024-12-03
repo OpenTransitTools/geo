ace94bdd28ab4f258a15151ff4d3fa5224e62d0e
Frank Purcell <fxpurcell@gmail.com>
2024-12-03 15:49:43 -0800

    fix: back to original (working) versions of postgis

---
+++ postgis/test.sh
@@ -0,0 +1,28 @@
+C=${1:-"N"}
+S=5
+if [[ $C == STK* ]]; then
+  ~/server-config/docker/nuke.sh ALL
+  S=600
+fi
+if [[ $C == ST* ]]; then
+  echo start postgis
+  docker compose down
+  sleep 2
+  nohup docker compose up &
+  sleep $S
+fi
+if [[ $C == S* ]]; then
+  echo start geoserver
+  cd ~/geo/geoserver
+  docker compose down
+  sleep 2
+  nohup docker compose up &
+  sleep $S
+  cd -
+fi
+
+docker ps
+
+~/geo/scripts/db/create.sh
+~/geo/scripts/data/rename.sh
+~/geo/scripts/db/restore.sh
