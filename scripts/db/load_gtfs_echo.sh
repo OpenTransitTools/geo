0720334a12db505a77d6d7c48f6d104c7c374940
Frank Purcell <fxpurcell@gmail.com>
2025-03-31 14:14:30 -0700

    fix: support poetry script

--- scripts/db/load_gtfs_echo.sh
+++ scripts/db/load_gtfs_echo.sh
@@ -5,11 +5,16 @@
 LDDIR=`dirname $0`
 . $LDDIR/../base.sh
 
+gtfs_load="poetry run gtfsdb-load"
+if [ -f "bin/gtfsdb-load" ]; then
+  gtfs_load="bin/gtfsdb-load"
+fi
+
 for f in ${GTFS_DIR}/*gtfs.zip
 do
   name=$(feed_name_from_zip $f)  
 
-  cmd="bin/gtfsdb-load -c -ct -g -d $otp_url -s ${name} ${f}"
+  cmd="$gtfs_load -c -ct -g -d $otp_url -s ${name} ${f}"
   echo $cmd
   #eval $cmd
 done
